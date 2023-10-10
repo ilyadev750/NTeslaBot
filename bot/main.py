0c3f41f0ab3219be32545e16e50dcc2b59ba1161
Ilya Petrov <ilyapetrov@Ilyas-Mac-mini.local>
2023-10-10 13:52:32 +0200

    add parse mode and delete master branch

--- bot/main.py
+++ bot/main.py
@@ -8,9 +8,15 @@ bot = Bot(MY_TOKEN)
 dp = Dispatcher(bot)
 
 
+@dp.message_handler(commands=['start'])
+async def echo(message: types.Message):
+    await message.answer('<b>Welcome to Nicola Tesla Airport bot. You could get information about arrivals and '
+                         'departures. Choose your options: </b>', parse_mode='HTML')
+
+
 @dp.message_handler()
 async def echo(message: types.Message):
-    await message.answer(text=message.text)
+    await message.answer('TEST WORK')
 
 if __name__ == '__main__':
     executor.start_polling(dp)
