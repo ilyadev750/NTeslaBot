f031967879b55f35a5cf0497c7a88db347d8f8d7
Ilya Petrov <ilyapetrov@Ilyas-Mac-mini.local>
2023-10-11 14:58:08 +0200

    bot is starting to work

--- bot/main.py
+++ bot/main.py
@@ -1,5 +1,5 @@
 from aiogram import Bot, Dispatcher, executor, types
-
+from NTeslaBot.parser.parser import Parser
 
 MY_TOKEN = "6542990215:AAErSHJUVLj2GEoheBrfyQ2WNrjssnLqZms"
 
@@ -9,9 +9,16 @@ dp = Dispatcher(bot)
 
 
 @dp.message_handler(commands=['start'])
-async def echo(message: types.Message):
+async def start(message: types.Message):
     await message.answer('<b>Welcome to Nicola Tesla Airport bot. You could get information about arrivals and '
                          'departures. Choose your options: </b>', parse_mode='HTML')
+
+
+@dp.message_handler(commands=['arrivals'])
+async def arrivals(message: types.Message):
+    my_parser = Parser(type_of_schedule='Departures', day='Today')
+    my_parser.run()
+    await message.answer(f'{my_parser.all_flights[0]}')
 
 
 @dp.message_handler()
