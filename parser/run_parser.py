21abac6e367ce40f306ac9aa99e8935d8cd669e2
Ilya Petrov <ilyapetrov@Ilyas-Mac-mini.local>
2023-10-15 16:39:48 +0200

    working with yield

--- parser/run_parser.py
+++ parser/run_parser.py
@@ -3,6 +3,10 @@ from parser import Parser
 if __name__ == '__main__':
     my_parser = Parser(type_of_schedule='Departures', day='Today')
     my_parser.run()
+    for flight in my_parser.all_flights:
+        info = my_parser.print_flight(flight=flight)
+        print(list(info))
+
 # my_parser = Parser(type_of_schedule='Departures', day='Tomorrow')
 # my_parser.run()
 # my_parser = Parser(type_of_schedule='Departures', day='Yesterday')
